acf1eb777b0ea3ab869f0807300f8172b0aab393
chayatat123 <chayatat.seer@bumail.net>
2026-03-27 17:05:45 +0700

    class

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -7,7 +9,50 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-
+        //InvokeRepeating(nameof (RandomSpawn),0,5);
+        StartCoroutine(SpawnRoutine());
     }
-
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[index];
+        Instantiate(enemyPrefab, spawnPoint.position,Quaternion.identity);
+    }
+   IEnumerator SpawnRoutine()
+    {
+            yield return new WaitForSeconds(5);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+    IEnumerator Goodbye()
+    {
+        while (true)
+        {
+            Debug.Log("Bye" + Time.frameCount + "" + Time.time);
+           //yield return new WaitForSeconds(1);
+           yield return null;
+           //StartCoroutine(Hello());
+           yield return Hello();
+        }
+    }
+     
+    IEnumerator Hello()
+    {
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        yield return null;
+        yield return null;
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+    }
 }
